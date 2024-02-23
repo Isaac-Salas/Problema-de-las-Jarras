377e31b75fa434e13ce8b0f48483d3a9f36a9092
Isaac Salas <iscprepa@gmail.com>
2024-02-23 11:47:54 -0600

    Adiciones faltantes
    Se agregan funciones faltanes y se agregan instrucciones de captrura de capacidad de las jarras y se intenta solucionar

--- Isaac-Salas-Jarras.cpp
+++ Isaac-Salas-Jarras.cpp
@@ -59,8 +59,91 @@
 	 }
 };
 
+//Funcion para encontrar el Maximo Comun Denominador
+int MCD (int n, int m) 
+{
+	if (m<=n && n%m == 0)
+	{
+		return m;
+	}
+	if (n < m)
+	{
+		return MCD(m,n);
+	}
+	else
+	{
+		return MCD(m,n%m);
+	}
+}
+
+// Verificar la posibilidad de la solucion
+bool chequeo (int a, int b, int c)
+{
+	// Detectectar si el valor que se quiere en una jarra excede la capacidad de A
+	if (c>a){
+		cout<<"La jarra A no puede ser llenada con mas que su capacidad";
+		return false;
+	}
+	
+	// Si c es multiplo de el > de a y de b, entonces es posible
+	if (c %MCD(a,b) == 0)
+	{
+		return true;
+	}
+	//Si c no es multiplo del ? de a y de b, entonces no es posible
+	cout << "No es posible llegar a ese estado con la capacidad de las jarras dadas";
+	return false;	
+}
+
+// Intentar resolver el problema
+void resolver (Jarra A, Jarra B, int resultado)
+{
+	while (A.ValorAct() != resultado)
+	{
+		// Posibles casos
+		
+		//1. A lleno - B vacio
+		if (!A.Lleno() && B.Vacio())
+		{
+			cout << "#Llenar B\n";
+			B.Llenar();
+			cout << "(A,B) = (" << A.ValorAct() << ", " << B.ValorAct() << ")\n";
+		}
+		//2. A Lleno
+		if (A.Lleno())
+		{
+			cout << "#Vaciar A\n";
+			A.Vaciar();
+			cout << "(A,B) = (" << A.ValorAct() << ", " << B.ValorAct() << ")\n";
+		}
+		
+		cout << "#Pasar agua de B -> A\n";
+		A[B];
+		cout << "(A,B) = (" << A.ValorAct() << ", " << B.ValorAct() << ")\n";
+	}
+}
+
 
 int main()
 {
+	int a, b, resutado;
+	cout << "Introduce la capacidad de A: \n";
+	cin >> a;
+	cout << "Introduce la capacidad de B: \n";
+	cin >> b;
+	
+	do{	
+	cout << "Introduce la capacidad deseada de A: \n";
+	cin >> resutado;
+	}
+	while(!chequeo(a,b,resutado));
+	
+	Jarra A(a), B(b);
+	
+	cout << "\n\n-Iniciar con: (A,B) = (" << A.ValorAct() << ", " << B.ValorAct() << ")\n\n";
+	resolver(A, B, resutado);
+	cout << "Listo!"<< endl;
+
+	
 	return 0;
 }
